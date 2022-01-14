29e43b1274fe98ce9acadc924e60da3786024843
Claudio-P <pclaudio435@gmail.com>
2022-01-14 16:40:38 -0300

    Feito o esqueleto do primeiro comando

--- bot.js
+++ bot.js
@@ -1,13 +1,17 @@
-const Discord = require('discord.js');
 const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES] });
 require('dotenv').config();
 
 const token = process.env.CLIENT_TOKEN;
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+client.once('ready', () => {
+    console.log('Ready!');
+});
 
-client.on("ready", () => {
-    console.log('Bot online!');
+client.on('message', message => {
+    if (message.content == "!start") {
+        message.reply('Pomodoro iniciado!');
+    }
 });
 
 client.login(token);

67f8c5903380d16393caca9800455c593a303712
Claudio-P <pclaudio435@gmail.com>
2022-01-14 19:36:55 -0300

    Comando pomodoro.

--- bot.js
+++ bot.js
@@ -1,16 +1,32 @@
 const { Client, Intents } = require('discord.js');
+const { timeout } = require('nodemon/lib/config');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES] });
 require('dotenv').config();
 
 const token = process.env.CLIENT_TOKEN;
 
+setTimeout(() => {
+
+}, 15);
+
 client.once('ready', () => {
     console.log('Ready!');
 });
 
-client.on('message', message => {
-    if (message.content == "!start") {
+client.on('messageCreate', async message => {
+    if (message.content.startsWith('!start')) {
         message.reply('Pomodoro iniciado!');
+
+        const content = message.content;
+        const convertedContent = content.split(' ');
+        const quantity = convertedContent[1];
+        const time = convertedContent[2];
+        const rest = convertedContent[3];
+
+        for (let i = 0; i < quantity; i++) {
+            setTimeout(async () => await message.channel.send('Descanço Iniciado!'), parseInt(time) * 60000);
+            setTimeout(async () => await message.channel.send('Pomodoro Iniciado!'), parseInt(rest) * 60000);
+        }
     }
 });
 

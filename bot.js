2d074d21e24c7bf3f38b6ff930f372d9b477126a
Lucas Vieira <lucasvieira.dev2020@gmail.com>
2022-01-14 21:48:18 -0300

    Refactored initial functions.

--- bot.js
+++ bot.js
@@ -5,28 +5,56 @@ require('dotenv').config();
 
 const token = process.env.CLIENT_TOKEN;
 
-setTimeout(() => {
+function firstCommand(message) {
+    const command = message.content.split(" ")
+    return command[0]
+}
 
-}, 15);
+function timeOut(ms) {
+    return new Promise(resolve => {
+        setTimeout(resolve, ms * 60000)
+    })
+}
+
+async function time(message, ms, botMessage) {
+    await timeOut(ms).then(() => {
+        message.channel.send(`🍅 ${botMessage}`)
+    })
+}
+
+async function startPomodoro(message) {
+    message.reply('🍅 _**Olá, Seja bem-vindo. Me chamo **Focus** sou um **PomoBot** 🤖**_');
+
+    const convert = message.content
+    const convertedContent = convert.split(' ');
+    const pomodoroQuantity = convertedContent[1];
+    const pomodoroTime = convertedContent[2];
+    const pomodoroRest = convertedContent[3];
+
+    message.reply(`⏲️ **Start Pomodoro!** \n🔄 **Você iniciou um Ciclo de ${pomodoroQuantity} Pomodoro** \n🕐 **Com uma duração de ${pomodoroTime}:00 min (_Cada Ciclo_)** \n🕐 **Seus intervalos de descanso são de ${pomodoroRest}:00 min**`);
+
+    for (let i = 0; i < pomodoroQuantity; i++) {
+        await time(message, parseInt(pomodoroTime), "**Descanso** 🥱")
+        await time(message, parseInt(pomodoroRest), "**Iniciando novamente** 🏁")
+    }
+
+    
+    console.log("Ended")
+
+}
+
 
 client.once('ready', () => {
     console.log('Ready!');
 });
 
 client.on('messageCreate', async message => {
-    if (message.content.startsWith('!start')) {
-        message.reply('Pomodoro iniciado!');
-
-        const content = message.content;
-        const convertedContent = content.split(' ');
-        const quantity = convertedContent[1];
-        const time = convertedContent[2];
-        const rest = convertedContent[3];
-
-        for (let i = 0; i < quantity; i++) {
-            setTimeout(async () => await message.channel.send('Descanço Iniciado!'), parseInt(time) * 60000);
-            setTimeout(async () => await message.channel.send('Pomodoro Iniciado!'), parseInt(rest) * 60000);
-        }
+    const command = firstCommand(message)
+    
+    switch (command) {
+        case '!start':
+            startPomodoro(message)
+            break;
     }
 });
 
